e660955756d80e9cec73bdeffa4ce998fc92e39e
Geoff Schmit <gcschmit@gmail.com>
2019-09-10 21:26:38 -0500

    finished stack, queue, and priority queue examples

--- Chapter 15 Class Notes/src/PriorityQueueDemo.java
+++ Chapter 15 Class Notes/src/PriorityQueueDemo.java
@@ -1,12 +1,26 @@
 import java.util.PriorityQueue;
+import java.util.Queue;
+
 
 /**
- * This program demonstrates a priority queue of work orders. The
- * most important work orders are removed first.
+ * This program demonstrates a priority queue of to-do items. The
+ * most important to-do items are removed first.
 */
 public class PriorityQueueDemo
 {
     public static void main(String[] args)
     {
+        // create a priority queue of to-do items
+        Queue<WorkOrder> q = new PriorityQueue<>();
+
+        q.add(new WorkOrder(3, "vacuum carpets"));
+        q.add(new WorkOrder(2, "water plants"));
+        q.add(new WorkOrder(10, "play video games"));
+        q.add(new WorkOrder(1, "take Chapter 15 practice quiz"));
+
+        while(q.size() > 0)
+        {
+            System.out.println(q.remove());
+        }
     }
 }
