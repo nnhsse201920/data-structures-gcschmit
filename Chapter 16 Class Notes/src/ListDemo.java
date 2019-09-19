f6dae0deb0dae89a3f2fa652b874fb4ad5287c92
Geoff Schmit <gcschmit@gmail.com>
2019-09-19 15:08:43 -0500

    implemented list iterator

--- Chapter 16 Class Notes/src/ListDemo.java
+++ Chapter 16 Class Notes/src/ListDemo.java
@@ -12,6 +12,24 @@ public class ListDemo
         staff.addFirst("Harry");
         staff.addFirst("Diana");
 
+        ListIterator iterator = staff.listIterator();   // |DHRT
+        iterator.next();                                // D|HRT
+        iterator.next();                                // DH|RT
 
+        // add more elements after the second element
+        iterator.add("Juliet");                         // DHJ|RT
+        iterator.add("Maria");                          // DHJM|RT
+
+        // remove Romeo
+        iterator.next();                                // DHJMR|T
+        iterator.remove();                              // DHJM|T
+
+        // print all elements
+        iterator = staff.listIterator();
+        while(iterator.hasNext())
+        {
+            System.out.print(iterator.next() + " ");
+        }
+        System.out.println();
     }
 }
