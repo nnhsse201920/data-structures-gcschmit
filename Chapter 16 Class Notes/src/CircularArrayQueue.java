7a33971c87ed842347f3e1f378e7c63209a638d0
Geoff Schmit <gcschmit@gmail.com>
2019-09-24 14:24:41 -0500

    implemented stack and queue

--- Chapter 16 Class Notes/src/CircularArrayQueue.java
+++ Chapter 16 Class Notes/src/CircularArrayQueue.java
@@ -6,51 +6,69 @@ import java.util.NoSuchElementException;
 public class CircularArrayQueue
 {
     private Object[] elements;
-    //private data
-
+    private int head;
+    private int tail;
+    private int currentSize;
 
 
     /**
         Constructs an empty queue.
     */
-
-
-
-
-
+    public CircularArrayQueue()
+    {
+        final int INITIAL_SIZE = 10;
+        this.elements = new Object[INITIAL_SIZE];
+        this.head = 0;
+        this.tail = 0;
+        this.currentSize = 0;
+    }
 
     /**
         Checks whether this queue is empty.
         @return true if this queue is empty
     */
-
-
-
+    public boolean empty()
+    {
+        return (this.currentSize ==  0);
+    }
 
     /**
         Adds an element to the tail of this queue.
         @param newElement the element to add
     */
+    public void add(Object newElement)
+    {
+        this.growIfNecessary();
 
-
-
-
+        this.currentSize++;
+        this.elements[this.tail] = newElement;
+        this.tail++;
+        this.tail %= this.elements.length;
+    }
 
     /**
         Removes an element from the head of this queue.
         @return the removed element
     */
+    public Object remove()
+    {
+        if(this.empty())
+        {
+            throw new NoSuchElementException();
+        }
 
-
-
-
+        Object removed = this.elements[this.head];
+        this.head = (this.head + 1) % this.elements.length;
+        this.currentSize--;
+        return removed;
+    }
 
     /**
         Grows the element array if the current size equals the capacity.
     */
     private void growIfNecessary()
     {
-        /*
+
         if(this.currentSize == this.elements.length)
         {
             Object[] newElements = new Object[2 * this.elements.length];
@@ -62,7 +80,7 @@ public class CircularArrayQueue
             this.head = 0;
             this.tail = this.currentSize;
         }
-         */
+
     }
 
 
--- Chapter 16 Class Notes/src/QueueDemo.java
+++ Chapter 16 Class Notes/src/QueueDemo.java
@@ -2,5 +2,26 @@ public class QueueDemo
 {
     public static void main(String[] args)
     {
+        CircularArrayQueue queue = new CircularArrayQueue();
+
+        queue.add("Tom");
+        queue.add("Diana");
+        queue.add("Harry");
+        queue.add("a");
+        queue.add("b");
+        queue.add("c");
+        queue.add("d");
+        queue.add("e");
+        queue.add("f");
+        queue.add("g");
+        queue.add("h");
+        queue.add("i");
+        queue.add("j");
+        queue.add("k");
+
+        while(!queue.empty())
+        {
+            System.out.println(queue.remove());
+        }
     }
 }
